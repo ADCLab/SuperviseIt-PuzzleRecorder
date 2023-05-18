086ea50a020deab4a2c3722ded5d9ad42e5de67d
sewell <sewelljohnathan@proton.me>
2023-05-18 19:24:31 -0400

    Make sure that the sheetsCredentials are available

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Check for the google sheets credentials json file
+if [[ ! -f sheetsCredentials.json ]]; then
+    echo sheetsCredentials.json not found.
+    exit 1
+fi
+
+# Generate executable
 pyinstaller -F -w --clean -n "Cluster Tracking" -i "TheTab_KGrgb_72ppi.ico" --add-data "TheTab_KGrgb_72ppi.png:." src/main.py
