97b7754836b70045559580111f71afcf782aea69
sewell <sewelljohnathan@proton.me>
2023-05-18 13:41:25 -0400

    Add mp4 files to clean script

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Removes all .log files, .bag files, .csv files, and snapshots,
-for file in $(ls | egrep "*.(log|bag|csv)|Snapshot[0-5].png"); do
+# Removes all .log files, .bag files, .csv files, snapshots, and mp4 files
+for file in $(ls | egrep "*.(log|bag|csv|mp4)|Snapshot[0-5].png"); do
     rm $file
 done
